ad14ef442290c50f324367462250d158fe491053
Pavel <114167889+Pavel365@users.noreply.github.com>
2023-02-23 00:18:07 +0200

    Add Lab

---
+++ OOP - Inheritance-Lab/CustomStack/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            StackOfStrings strings = new StackOfStrings();
+
+            Console.WriteLine(strings.IsEmpty());
+
+            strings.AddRange(new List<string>() { "1", "2", "3", "4", "5", "6" });
+
+            Console.WriteLine(strings.IsEmpty());
+        }
+    }
+}
