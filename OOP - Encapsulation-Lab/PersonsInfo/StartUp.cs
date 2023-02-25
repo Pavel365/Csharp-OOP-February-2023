44f08ca5a02fe5c795a94261efbfafd1734145d0
Pavel <114167889+Pavel365@users.noreply.github.com>
2023-02-25 21:41:51 +0200

    Add Lab

---
+++ OOP - Encapsulation-Lab/PersonsInfo/StartUp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+
+            List<Person> persons = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                try
+                {
+                    string[] cmdArgs = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                    Person person = new Person(cmdArgs[0], cmdArgs[1],
+                        int.Parse(cmdArgs[2]), decimal.Parse(cmdArgs[3]));
+
+                    persons.Add(person);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            decimal parcentage = decimal.Parse(Console.ReadLine());
+
+            persons.ForEach(p => p.IncreaseSalary(parcentage));
+
+            persons.ForEach(p => Console.WriteLine(p.ToString()));
+
+            Team team = new Team("SoftUni");
+
+            foreach (Person person in persons)
+            {
+                team.AddPlayer(person);
+            }
+        }
+    }
+}
